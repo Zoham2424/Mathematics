280bb6a449489ec0d6abac3b43f2bbc8f49a759b
zoham2424 <iaalvaradocubero@email.neit.edu>
2025-01-09 19:37:45 -0500

    New info

---
+++ Mathematics.Test/BasicMathTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mathematics.Test
+{
+     public class BasicMathTest
+    {
+        //[Fact]
+        [Theory]
+        [InlineData(5,3,8)]
+        [InlineData(10,3,13)]
+
+        public void TestAddNumbers(int num1, int num2, int expectedResult) 
+        { 
+         var math = new BasicMath();
+            var result = math.AddNumbers(num1, num2);
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void TestSubtractNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.SubtractNumbers(10, 3);
+            Assert.True(result == 7);
+        }
+        [Fact]
+        public void TestMultiplyNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.MultiplyNumbers(5, 6);
+            Assert.True(result == 30);
+        }
+        [Fact]
+        public void TestDivideNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.DivideNumbers(50, 5);
+            Assert.True(result == 10);
+        }
+
+    }
+}
