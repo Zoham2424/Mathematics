47f2aa0df8db6def5ec766971abe27cbacc9c120
Ian A <iaalvaradocubero@email.neit.edu>
2025-02-05 10:09:03 -0500

    s

--- Mathematics.Test/obj/Debug/net8.0/Mathematics.Test.AssemblyInfo.cs
+++ Mathematics.Test/obj/Debug/net8.0/Mathematics.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mathematics.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35f9a9ddbf4972589cdd6a17a0e7f8153e67d24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb93eee39bced0e8e371aebc19620c97b2bf9403")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mathematics.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mathematics.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MathematicsConsole.ConsoleApp/Program.cs
+++ MathematicsConsole.ConsoleApp/Program.cs
@@ -14,8 +14,11 @@ namespace Mathmatics.ConsoleApp
                 ValidateArguments(args, out operation, out operand1, out operand2);
 
                 BasicMath basicMath = new BasicMath();
-                double result;
-                string operationDesc;
+                AdvMath advMath = new AdvMath();
+                double result = 0;
+                string operationDesc = "";
+                string advOperation = "";
+                double advResult = 0;
 
                 switch (operation)
                 {
@@ -43,20 +46,54 @@ namespace Mathmatics.ConsoleApp
                             operationDesc = "divided by";
                             break;
                         }
+                    case "area":
+                        {
+                            advResult = advMath.CalcArea(operand1, operand2);
+                            advOperation = "area with dimensions";
+                            break;
+                        }
+                    case "avg":
+                        {
+                            List<double> values = new List<double> { operand1, operand2 };
+                            advResult = advMath.CalcAverage(values);
+                            advOperation = "average of";
+                            break;
+                        }
+                    case "sqr":
+                        {
+                            advResult = advMath.Squared(operand1);
+                            advOperation = "squared value of";
+                            operand2 = 0;
+                            break;
+                        }
+                    case "hyp":
+                        {
+                            advResult = advMath.CalcHypotenuse(operand1, operand2);
+                            advOperation = "hypotenuse with sides";
+                            break;
+                        }
                     default:
                         {
                             throw new Exception("An unexpected error occurred");
                         }
                 }
 
-                Console.WriteLine($"{operand1} {operationDesc} {operand2} is equal to {result}");
+                if (advOperation != null)
+                {
+                    Console.WriteLine($"{operand1} {advOperation} {operand2} is equal to {advResult}");
+                }
+                else
+                {
+                    Console.WriteLine($"{operand1} {operationDesc} {operand2} is equal to {result}");
+                }
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"[ERROR] Something went wrong: {ex.Message}");
             }
-        }
 
+        }
 
         private static void ValidateArguments(string[] args, out string operation, out int operand1, out int operand2)
         {
@@ -65,9 +102,10 @@ namespace Mathmatics.ConsoleApp
                 throw new ArgumentOutOfRangeException("Not enough arguments were passed to perform the operation.");
             }
 
-            if (args[0] != "+" && args[0] != "-" && args[0] != "*" && args[0] != "/")
+            if (args[0] != "+" && args[0] != "-" && args[0] != "*" && args[0] != "/" &&
+                args[0] != "area" && args[0] != "avg" && args[0] != "sqr" && args[0] != "hyp")
             {
-                throw new ArgumentException("First argument must be a math operator.");
+                throw new ArgumentException("First argument must be a valid math operator.");
             }
 
             if (!int.TryParse(args[1], out operand1))
@@ -75,7 +113,7 @@ namespace Mathmatics.ConsoleApp
                 throw new ArgumentException("Second argument must be a valid integer.");
             }
 
-            if (!int.TryParse(args[2], out operand2))
+            if (!int.TryParse(args[2], out operand2) && args[0] != "sqr")
             {
                 throw new ArgumentException("Third argument must be a valid integer.");
             }
